e95188b8571831da615bc10ba56d386a4a4af398
Dembezum <dembezuuma@gmail.com>
2023-05-17 10:09:06 +0200

    Update treesitter

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,16 +1,16 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
     ensure_installed = {
-    "python",
-    "javascript",
-    "rust",
-    "bash",
-    "java",
-    "c",
-    "lua",
-    "vim",
-    "vimdoc"
-},
+        "python",
+        "javascript",
+        "rust",
+        "bash",
+        "java",
+        "c",
+        "lua",
+        "vim",
+        "vimdoc"
+    },
     ignore_install = { "" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
@@ -18,5 +18,11 @@ require 'nvim-treesitter.configs'.setup {
     auto_install = true,
     highlight = {
         enable = true,
+        disable = {
+            "clangd",
+            "markdown",
+            "cpp",
+            "lua_ls",
+        }
     },
 }
